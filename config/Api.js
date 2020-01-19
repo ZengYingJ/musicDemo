26cc680a514536928389edd7e997c2ad3aad660f
J <zengyj@whatyouneed.cc>
2020-01-19 18:55:05 +0800

    新增api参数注释

--- config/Api.js
+++ config/Api.js
@@ -12,24 +12,24 @@ module.exports = {
     // 歌手类型列表
     getSingerTypeList: `${Main.Host.mkugou}/singer/class&json=true`,
 
-    // 歌单的歌曲列表
+    // 歌单的歌曲列表   /plist/list/歌单id?json=true
     getSongListDetail: `${Main.Host.mkugou}/plist/list/`,
 
-    // 歌单排行榜的歌曲列表
+    // 歌单排行榜的歌曲列表     /rank/info/?rankid=排行榜id&page=页码&json=true
     getRankListDetail: `${Main.Host.mkugou}/rank/info/`,
 
-    // 歌手列表
+    // 歌手列表     singer/list/歌手类型id?json=true&page=页码
     getSingerList: `${Main.Host.mkugou}/singer/list/`,
 
-    // 歌手歌曲列表
+    // 歌手歌曲列表     singer/info/歌手id&json=true&page=页码
     getSingerDetail: `${Main.Host.mkugou}/singer/info/`,
 
-    // 热门搜索列表
+    // 热门搜索列表     search/hot?format=json&plat=0&count=总数
     getHotSearchList: `${Main.Host.mobilecdn}/api/v3/search/hot`,
 
-    // 歌曲搜索
+    // 歌曲搜索     search/song?format=json&keyword=关键词&page=页码&pagesize=一页的数量&showtype=1
     getSongBySearch: `${Main.Host.mobilecdn}/api/v3/search/song`,
     
-    // 歌曲信息
+    // 歌曲信息     index.php?r=play/getdata&hash=歌曲hash
     getSongInfo: `${Main.Host.wkugou}/yy/index.php`,
 }
